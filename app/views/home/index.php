a2e2a6c29c709054dcb08997b3bbe41f0d72ff31
Uliya Faris <ulyafariz@gmail.com>
2021-06-15 15:59:29 +0700

    up3

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,32 +1,227 @@
 <?php
+
 ?>
-
-<!doctype html>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+<header>
+    <a href="#" class="logo">BLOG.ME</a>
+    <div class="toggle"></div>
+    <ul class="menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Content</a></li>
+        <li><a href="#work">My Design</a></li>
+        <li><a href="#testimonial">Writing</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</header>
+<section class="banner" id="home">
+    <div class="textBx">
+        <h2>Annyeong ! <br><span> Welcome to My Blog</span></h2>
+        <!-- <h3>See the World from a Point of View</h3> -->
+        <a href="#about" class="btn">About Me</a>
+    </div>
+</section>
+<section class="about" id="about">
+    <div class="heading">
+        <h2>About Me</h2>
+    </div>
+    <div class="content">
+        <div class="contentBx">
+            <h3>Halo !</h3>
+            <p>Saya adalah seorang beginner di dunia IT tepatnya mahasiswa
+                Fakultas Ilmu Komputer di salah satu Universitas di Indonesia.
+                <br><br>I didn't really think about getting into the IT world, actually
+                i want to major in business.</p>
+        </div>
+        <div class="w50">
+            <img src="gambar/about.jpg" >
+        </div>
 
-<!-- Optional JavaScript; choose one of the two! -->
+    </div>
+    </div>
+</section>
+<section class="services" id="services">
+    <div class="heading white">
+        <h2>My Content</h2>
+        <!-- <p>A little story about my hobby</p> -->
+    </div>
+    <div class="content">
+        <!-- <div class="servicesBx">
+            <img src="gambar/icon1.png" alt="">
+            <h2>Web Design</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Modi quo beatae voluptate aperiam iusto officia
+                assumenda corrupti nemo ipsum itaque reprehenderit</p>
+        </div> -->
+        <div class="servicesBx">
+            <img src="gambar/icon1.png" alt="">
+            <h2>Web Design</h2>
+            <p>Saya mencoba design pertama saya kemudian tertarik dalam dunia Design
+                dan sekarang masih tahap belajar
+            </p>
+        </div>
+        <!-- <div class="servicesBx">
+            <img src="gambar/icon2.png" alt="">
+            <h2>Web Development</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Modi quo beatae voluptate aperiam iusto officia
+                assumenda corrupti nemo ipsum itaque reprehenderit</p>
+        </div> -->
+        <!-- <div class="servicesBx">
+            <img src="gambar/icon3.png" alt="">
+            <h2>Android App</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Modi quo beatae voluptate aperiam iusto officia
+                assumenda corrupti nemo ipsum itaque reprehenderit</p>
+        </div> -->
+        <div class="servicesBx">
+            <img src="gambar/icon4.png" alt="">
+            <h2>Photography</h2>
+            <p>Saya menyukai Fotografi setiap melihat good view yang pas
+                saya mengambil objek gambar untuk fotografi </p>
+        </div>
+        <div class="servicesBx">
+            <img src="gambar/icon5.png" alt="">
+            <h2>Content Writing</h2>
+            <p>Saat merasa bosan dengan tugas kuliah kadang saya membuat content writer puisi</p>
+        </div>
+        <div class="servicesBx">
+            <img src="gambar/icon6.png" alt="">
+            <h2>Video Editing</h2>
+            <p>Video editing juga merupakan salah satu hobi yang tidak terlewatkan</p>
+        </div>
+    </div>
+</section>
+<section class="work" id="work">
+    <div class="heading">
+        <h2>My Design</h2>
+        <p>About My Design</p>
+    </div>
+    <div class="content">
+        <div class="workBx">
+            <img src="gambar/post.jpg" alt="">
+        </div>
+        <div class="workBx">
+            <img src="gambar/post2.jpg" alt="">
+        </div>
+        <div class="workBx">
+            <img src="gambar/post3.png" alt="">
+        </div>
+        <div class="workBx">
+            <img src="gambar/post4.png" alt="">
+        </div>
+    </div> <div class="heading">
+        <a href="#" class="btn">View More</a>
+    </div>
+</section>
+<section class="testimonial" id="testimonial">
+    <div class="heading">
+        <h2>Puisi</h2>
+        <!-- <p>writing</p> -->
+    </div>
+    <div class="content">
+        <div class="testimonialBx">
+            <p> <br> <b>Paku Patah</b><br> Sudah berapa kali  mengeluh,
+                sudah berapa lama bertahan,
+                sudah berapa lama tak berdaya,
+                masih ingatkah berapa kali berjuang,
+                hingga kita ucapkan kata eluh sepanjang waktu
+                masih ingatkah berapa kali jatuh
+                hingga berkata aku tidak sanggup bertahan...
+                masih ingatkah anda berapa kali berjuang
+                sampai kata tak berdaya keluar dari mulut kita..
+                ini semua tentang diri kita
+                coba tanya masing-masing diri kita.
+                apa peran kita sebenarnya...</p>
+            <h3>02 Mei 2020 15.33 WIB<br><span>Selamat Hari Pendidikan Nasional</span></h3>
+        </div>
+        <div class="testimonialBx">
+            <p><br><b>Alur</b><br> Mengalirlah...
+                Hanya mengikuti alur hidup
+                Ikutilah...
+                Menerima kebenaran taqdir
+                Telusurilah
+                Mutiara itu tidak didepan mata
+                Temukanlah
+                Hati akan bersajak bahagia
+                Junjunglah,Dapatkan!!
+                Jikalau hidupmu melurus
+                Kau tidak hidup</p>
+            <h3>11 April 2018 08.04 WIB<br><span>Banyuwangi ku</span></h3>
+        </div>
+    </div>
+</section>
+<section class="contact" id="contact">
+    <div class="heading white">
+        <h2>Contact</h2>
+        <p>You can call me</p>
+    </div>
+    <div class="content">
+        <div class="contactInfo">
+            <h3>Contact Info</h3>
+            <div class="contactInfoBx">
+                <div class="box">
+                    <div class="icon">
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+                    <div class="text">
+                        <h3>Address</h3>
+                        <p>1234 syurga,<br>Jannah,Langit,<br>54321</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <div class="text">
+                        <h3>Phone</h3>
+                        <p>085706986900</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon">
+                        <i class="fa fa-envelope-o"></i>
+                    </div>
+                    <div class="text">
+                        <h3>Email</h3>
+                        <p>ulyafariz@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="formbx">
+            <form>
+                <h3>Message Me</h3>
+                <input type="text" name="" placeholder="Full Name">
+                <input type="email" name="" placeholder="Email">
+                <textarea placeholder="Your Message"></textarea>
+                <input type="submit" value="Send">
+            </form>
+        </div>
+    </div>
+</section>
+<div class="copyright">
+    <p>Copyright © 2021 Blog.Me.All Right Reserved</p>
+</div>
+<script type="text/javascript">
+    window.addEventListener('scroll',function(){
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY >0);
+    });
 
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
--->
+</script>
+
 </body>
 </html>
