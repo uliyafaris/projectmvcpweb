7389493dc40dda98edf67f0e857208792c1f670b
Uliya Faris <ulyafariz@gmail.com>
2021-06-18 14:07:58 +0700

    up8

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -35,12 +35,12 @@
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
-                <div id="emailHelp" class="form-text"><a href="admin/index.php">Forgot Password?</a></div>
+                <div id="emailHelp" class="form-text"><a style="text-decoration: None; " href="index.php">Forgot Password?</a></div>
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                <br><br><button type="submit" class="btn btn-success">Login</button>
+                <br><br><button type="submit" class="btn btn-success"><a style="text-decoration: None; color: white" href="../home/index.php">Login</a></button>
             </div>
 
         </form>
